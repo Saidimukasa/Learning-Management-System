53cbc482f833503af8463708a5ef0d89eea087e8
cartel360 <billycartel360@gmail.com>
2022-03-10 17:31:32 +0300

    Added Host

--- LMS/settings.py
+++ LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i)d0thg)h(qo(_=gnjf)0ho^7cfhwy@@gt#p#3zi_+1hq+ax)@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['learning-ms.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
